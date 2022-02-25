c8ff51a8698699fd781746cfd135ebd3df251062
dmkimya <100382564+dmkimya@users.noreply.github.com>
2022-02-26 00:17:06 +0300

    Update tahsilat.vue

--- pages/admin/tahsilat.vue
+++ pages/admin/tahsilat.vue
@@ -24,7 +24,7 @@
 export default {
   data() {
     return {
-      workers: ["yonetim", "fikri, "Toplam"],
+      workers: ["yonetim", "fikri", "Toplam"],
       workerCollectedList: []
     };
   },
